13d95548d1082a6e08c4ca9f6e62b2bb3ea0d983
Aryaswan17 <aryaswan17@gmail.com>
2021-07-18 17:15:19 +0800

    fix trapezoid transition

---
+++ script.js
@@ -0,0 +1,16 @@
+function openNav() {
+  document.getElementById("ultra-nav").style.left = "0px";
+  document.getElementById("openNav").style.left = "-50px";
+};
+
+function closeNav() {
+  document.getElementById("ultra-nav").style.left = "-223px";
+  document.getElementById("openNav").style.left = "5px";
+};
+
+function trapezoidanim() {
+	document.getElementById("slanted-box-left").style.left = "0px";
+	document.getElementById("slanted-box-right").style.left = "0px";
+};
+
+trapezoidanim()
