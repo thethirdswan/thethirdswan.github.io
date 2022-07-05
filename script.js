ecde3f1a0bde5bae3379f1527f2361a30b4033b0
thethirdswan <aryaswan17@gmail.com>
2022-07-05 21:30:17 +0800

    added more features

--- script.js
+++
@@ -1,6 +0,0 @@
-function trapezoidanim() {
-	document.getElementById("slanted-box-left").style.left = "0px";
-	document.getElementById("slanted-box-right").style.left = "0px";
-};
-
-trapezoidanim()
